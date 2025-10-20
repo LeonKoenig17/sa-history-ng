6805e5ad943c6ffde0c74fd5c28d91fe6a9b4868
Beslim17 <b.montiari@gmail.com>
2025-10-20 18:43:58 +0200

    test

---
+++ src/app/main/events/single-event/single-event.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-single-event',
+  standalone: true,
+  imports: [],
+  templateUrl: './single-event.component.html',
+  styleUrl: './single-event.component.scss'
+})
+export class SingleEventComponent {
+
+}
